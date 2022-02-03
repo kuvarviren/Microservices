7edb24d547562403a1373343835833ea8655939c
kuvarviren <kuvarviren@gmail.com>
2022-02-04 00:34:31 +0530

    Implemented CouponAPI Microservice

---
+++ Mango.Services.CouponAPI/Controllers/CouponController.cs
@@ -0,0 +1,34 @@
+﻿using Mango.Services.CouponAPI.Models.Dto;
+using Mango.Services.CouponAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.CouponAPI.Controllers
+{
+    [ApiController]
+    [Route("api/coupons")]
+    public class CouponController : Controller
+    {
+        private readonly ICouponRepository _couponRepository;
+        protected ResponseDto _response;
+        public CouponController(ICouponRepository couponRepository)
+        {
+            _couponRepository = couponRepository;
+            _response = new ResponseDto();
+        }
+        [HttpGet("{code}")]
+        public async Task<object> GetDiscountForCode(string code)
+        {
+            try
+            {
+                CouponDto couponDto = await _couponRepository.GetCouponByCode(code);
+                _response.Result = couponDto;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.Message };
+            }
+            return _response;
+        }
+    }
+}
---
+++ Mango.Services.CouponAPI/Repository/CouponRepository.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Mango.Services.CouponAPI.DbContexts;
+using Mango.Services.CouponAPI.Models.Dto;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.CouponAPI.Repository
+{
+    public class CouponRepository : ICouponRepository
+    {
+        private readonly AppDbContext _db;
+        protected IMapper _mapper;
+        public CouponRepository(AppDbContext db, IMapper mapper)
+        {
+            _db = db;
+            _mapper = mapper;
+        }
+
+        public async Task<CouponDto> GetCouponByCode(string couponCode)
+        {
+            var couponFromDb = await _db.Coupons.FirstOrDefaultAsync(u => u.CouponCode == couponCode);
+            return _mapper.Map<CouponDto>(couponFromDb);
+        }
+    }
+    
+}
